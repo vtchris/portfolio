6ef4ad2f72abbcf203c3661fa16f60520e7a35b2
VTC <vtchris@hotmail.com>
2019-07-12 15:50:52 -0400

    Added js for portfolio and about pages

--- assets/javascript/objects.js
+++ assets/javascript/objects.js
@@ -1,20 +1,20 @@
 const PROJECTS = [];
 
 PROJECTS.push(new Project(
-    "Game of Thrones Hangman",
-    "JavaScript word guessing game in which you guess the names of characters from Game of Thrones!",
-    "got.jpg",
-    "Game of Thrones Hangman",
-    "https://github.com/vtchris/hangman",
-    "https://vtchris.github.io/hangman/"
+    "Grub Whiz",
+    "This resturant finding application uses APIs and was developed using agile methodology during a two week period on a team of four.",
+    "GrubWiz.jpg",
+    "Grub Whiz Restuarant Finder",
+    "https://github.com/vtchris/project1",
+    "https://vtchris.github.io/project1/"
 ));
 PROJECTS.push(new Project(
-    "X-Men RPG Game",
-    "JavaScript role-playing game based on the X-Men, utilizing jQuery for DOM interactions.",
-    "x.jpg",
-    "X-Men Role Playing Game",
-    "https://github.com/vtchris/unit-4-game",
-    "https://vtchris.github.io/unit-4-game/"
+    "Rock Paper Scissors",
+    "Multi-player Rock Paper Scissors game built with JavaScript and Firebase Database.",
+    "rps.jpg",
+    "Rock Paper Scissors",
+    "https://github.com/vtchris/RPS-Multiplayer",
+    "https://vtchris.github.io/RPS-Multiplayer/"
 ));
 PROJECTS.push(new Project(
     "Sentenced: Trivia Game",
@@ -25,6 +25,14 @@ PROJECTS.push(new Project(
     "https://vtchris.github.io/TriviaGame/"
 ));
 PROJECTS.push(new Project(
+    "X-Men RPG Game",
+    "JavaScript role-playing game based on the X-Men, utilizing jQuery for DOM interactions.",
+    "x.jpg",
+    "X-Men Role Playing Game",
+    "https://github.com/vtchris/unit-4-game",
+    "https://vtchris.github.io/unit-4-game/"
+));
+PROJECTS.push(new Project(
     "Giffy Gallery",
     "JavaScript application using an API to display giffs which can be played and paused via clicking.",
     "gallery.jpg",
@@ -33,16 +41,22 @@ PROJECTS.push(new Project(
     "https://vtchris.github.io/Giffy-Gallery/"
 ));
 PROJECTS.push(new Project(
-    "Rock Paper Scissors",
-    "Multi-player Rock Paper Scissors game built with JavaScript and Firebase Database.",
-    "rps.jpg",
-    "Rock Paper Scissors",
-    "https://github.com/vtchris/RPS-Multiplayer",
-    "https://vtchris.github.io/RPS-Multiplayer/"
+    "Game of Thrones Hangman",
+    "JavaScript word guessing game in which you guess the names of characters from Game of Thrones!",
+    "got.jpg",
+    "Game of Thrones Hangman",
+    "https://github.com/vtchris/hangman",
+    "https://vtchris.github.io/hangman/"
 ));
-
-
-
+PROJECTS.push(new Project(
+    "Bamazon Node Store",
+    "This application was developed with Node.js and a MySql backend. It utilizes inquirer for user interaction.",
+    "bamazon.jpg",
+    "Bamazon Node Store",
+    "https://github.com/vtchris/bamazon-node-app",
+    "https://github.com/vtchris/bamazon-node-app"
+));
+//Project constructor
 function Project(name, description, image, imageAlt, url_gitrepo, url) {
     this.name = name;
     this.description = description;
--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,8 +1,136 @@
+//Global Variables
 const container = document.getElementById("container-div")
 
+//Add Navigation Listeners
+document.addEventListener('click', function (event) {
+
+    // Do nothing if target doesn't have specified class
+    if (!event.target.matches('.js_nav')) return;
+
+    event.preventDefault();
+
+    switch (event.target.innerText.toLowerCase()){
+        case "about":
+            clear_container();
+            display_about();
+        break;
+        case "contact":
+            clear_container();
+            display_contact();
+        break;
+        case "portfolio":
+            clear_container();
+            display_portfolio();
+        break;
+    }	    
+
+}, false);
+
+//Fire up the default page
 display_portfolio()
 
+function display_about(){
+    
+    //Create page header
+    let header = document.createElement('h2');
+    header.classList.add('mb-3');
+    header.innerText = 'About'
+    container.appendChild(header);
 
+    //Create row
+    let row =  document.createElement('div');
+    row.classList.add('row');
+
+    //Create 1st Col
+    let col1  =  document.createElement('div');    
+    col1.classList.add('col-md-3');
+
+    //Add Image
+    let img = document.createElement('img');
+    img.classList.add('img-thumbnail');
+    img.classList.add('mb-4');
+    img.setAttribute('src','assets/images/personal/chris.jpg');
+    img.setAttribute('id','imgChris')
+    col1.appendChild(img);
+    row.appendChild(col1);
+
+    //Create 2nd Col
+    let col2 =  document.createElement('div');  
+    col2.classList.add('col-md-5');
+
+    //Add paragraphs
+    let p1 = document.createElement('p');
+    p1.innerText = "I'm Chris Ross - I enjoy web development and I'm currently in a Coding Bootcamp at the Unicversity of North Carolina Charlotte to improve my skills. I like to create sites with a clean, uncluttered, and intuitive look. I look forward to strengthening and improving my skill set!"
+    col2.appendChild(p1)
+    let p2 = document.createElement('p');
+    p2.innerText = "I am originally from Vermont and have lived and worked in several cities and states in New England. I recently moved to South Carolina near Charlotte, NC. I also enjoy traveling, and as a glutten for punishment, I attempt to play the violin."
+    col2.appendChild(p2);
+    row.appendChild(col2);
+    
+    //Create 3rd Col
+    let col3 =  document.createElement('div');   
+    col3.classList.add('col-md-3');
+
+    //Add Contact card
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.classList.add('border-grey');
+    card.classList.add('mb-5');
+
+    let cardHeader = document.createElement('div');
+    cardHeader.classList.add('card-header');
+    cardHeader.innerText = "Connect";
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    //Github Button
+    let gitBtn = document.createElement('a');
+    gitBtn.classList.add('btn');
+    gitBtn.classList.add('btn-info');
+    gitBtn.classList.add('text-light');   
+    gitBtn.classList.add('btn-block');
+    gitBtn.setAttribute('href','https://github.com/vtchris');
+    gitBtn.setAttribute('target','_blank');
+    gitBtn.innerHTML = "<span class='fab fa-github mr-1'></span>Follow on Github!";
+    cardBody.appendChild(gitBtn);
+
+    //LinkedIn Button
+    let linkBtn = document.createElement('a');
+    linkBtn.classList.add('btn');
+    linkBtn.classList.add('btn-info');
+    linkBtn.classList.add('text-light');
+    linkBtn.classList.add('btn-block');   
+    linkBtn.setAttribute('href','https://www.linkedin.com/in/chrisross2357/');
+    linkBtn.setAttribute('target','_blank');
+    linkBtn.innerHTML = "<span class='fab fa-linkedin mr-1'></span>View LinkedIn profile!";
+    cardBody.appendChild(linkBtn);
+
+    //Twitter Button
+    let twitterBtn = document.createElement('a');
+    twitterBtn.classList.add('btn');
+    twitterBtn.classList.add('btn-info');
+    twitterBtn.classList.add('text-light');
+    twitterBtn.classList.add('btn-block');
+    twitterBtn.setAttribute('href','https://twitter.com/vtchris_2357');
+    twitterBtn.setAttribute('target','_blank');
+    twitterBtn.innerHTML = "<span class='fab fa-twitter mr-1'></span>Follow on Twitter!";
+    cardBody.appendChild(twitterBtn);
+        
+    card.appendChild(cardHeader);
+    card.appendChild(cardBody);
+    col3.appendChild(card);
+    
+    row.appendChild(col3);
+
+    container.appendChild(row);
+
+}
+function display_contact(){
+    let header = document.createElement('h2');
+    header.classList.add('mb-3');
+    header.innerText = 'Contact'
+    container.appendChild(header);
+}
 function display_portfolio() {
 
     let header = document.createElement('h2');
@@ -14,19 +142,17 @@ function display_portfolio() {
     let cardDeck
 
     for (let i = 0; i < PROJECTS.length; i++) {
-
         
-        //debugger;
         if (cardDeck) {
-            //if cardCount is 0 or divisible by 3 create card deck
-            if (cardCount === 0 || cardCount % 3 === 0) {
+            //if cardCount is 0 or divisible by 4 create card deck
+            if (cardCount === 0 || cardCount % 4 === 0) {
                 //If a card deck already exists, append it to the container
                 container.appendChild(cardDeck);
 
                 cardDeck = undefined
                 cardDeck = document.createElement('div');
-            cardDeck.classList.add('card-deck');
-            cardDeck.classList.add('mb-5');
+                cardDeck.classList.add('card-deck');
+                cardDeck.classList.add('mb-5');
 
             }
 
@@ -38,38 +164,56 @@ function display_portfolio() {
 
         }
 
-
+        //Create Repository card
         let card = document.createElement('div');
         card.classList.add('card');
 
+        //Link image to website / github
         let cardLink = document.createElement('a');
-        cardLink.setAttribute('href',PROJECTS[i].url);
-        cardLink.setAttribute('target','_demo');
+        cardLink.setAttribute('href', PROJECTS[i].url);
+        cardLink.setAttribute('target', '_demo');
         let img = document.createElement('img');
         img.classList.add('card-img-top');
-        img.setAttribute('src',`assets/images/${PROJECTS[i].image}`);
+        img.setAttribute('src', `assets/images/${PROJECTS[i].image}`);
         cardLink.appendChild(img)
         card.appendChild(cardLink);
 
+        //Add card body with description
         let cardBody = document.createElement('div');
         cardBody.classList.add('card-body');
-
-
         let cardHeader = document.createElement('h5');
         cardHeader.classList.add('card-title');
         cardHeader.innerText = PROJECTS[i].name;
-
-        let cardP =  document.createElement('p');
+        let cardP = document.createElement('p');
         cardP.classList.add('card-text');
         cardP.innerText = PROJECTS[i].description;
 
+        //Add footer with github repo link
+        let cardFooter = document.createElement('footer');
+        cardFooter.classList.add('card-footer');
+        cardFooter.classList.add('text-center');
+
+        let gitButton = document.createElement('a');
+        gitButton.classList.add('github-button');
+        gitButton.setAttribute('href',PROJECTS[i].url_gitrepo);
+        gitButton.setAttribute('data-size','large');
+        gitButton.innerText = "View Git Repo!"
+        cardFooter.appendChild(gitButton);
+
         cardBody.appendChild(cardHeader);
         cardBody.appendChild(cardP);
+        
         card.appendChild(cardBody);
+        card.appendChild(cardFooter);
         cardDeck.appendChild(card);
 
         cardCount++
     }
 
     container.appendChild(cardDeck)
+}
+function clear_container(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 }
